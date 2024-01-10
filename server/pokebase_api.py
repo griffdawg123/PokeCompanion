c62dc328f5d1f8d28fc925f7847f3c17e94a25d5
griffdawg123 <doylegriffin32@gmail.com>
2024-01-10 19:04:08 +1100

    API to get ID - need to improve for speed

--- server/pokebase_api.py
+++ server/pokebase_api.py
@@ -3,3 +3,11 @@ import pokebase as pb
 def list_of_pokemon_names():
     pokemon = pb.APIResourceList("pokemon")
     return [name for name in pokemon.names]
+
+def pokemon_dictionary():
+    pokemon_names = list_of_pokemon_names()[:152]
+    dex = {}
+    for name in pokemon_names:
+        print(name)
+        dex[pb.pokemon(name).order] = name
+    return dex
--- server/server.py
+++ server/server.py
@@ -1,5 +1,5 @@
 from flask import Flask
-from pokebase_api import list_of_pokemon_names
+from pokebase_api import list_of_pokemon_names, pokemon_dictionary
 
 app = Flask(__name__)
 
@@ -10,6 +10,10 @@ def hello():
 @app.route("/pokemon")
 def pokemon():
     return list_of_pokemon_names()
+
+@app.route("/pokedex")
+def pokedex():
+    return pokemon_dictionary()
 
 if __name__ == "__main__":
     app.run(debug=True)
