823424d6dba9cd1ca42a98cdd91f1e20ae6326bf
griffdawg123 <doylegriffin32@gmail.com>
2024-01-12 11:46:55 +1100

    basic stuff:

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,35 +1,37 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import './styles.css'
+import logo from './static/header.png'
+import { useEffect, useState } from 'react'
+import kanto from './static/151.json'
 
-function App() {
-  const [count, setCount] = useState(0)
+const MAX_WIDTH = 500
+
+export default function App() {
+
+  const  [width, setWidth] = useState(window.innerWidth)
+  const [toggleView, setToggleView] = useState(true)
+
+
+  useEffect(() => {
+    const handleResize = () => setWidth(window.innerWidth)
+    window.addEventListener("resize", handleResize)
+    return () => window.removeEventListener("resize", handleResize)
+  }, [])
+
+  const staticSize = width >= MAX_WIDTH
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div className="companion">
+    <img src={logo} alt="header image" width={staticSize ? MAX_WIDTH : width-80}></img>
+    <form className='filter-pokemon'>
+      <input type="text" placeholder="Enter a Pokemon's name..."></input>
+      <button className='filter-button'>Filter</button>
+    </form>
+    <div className='toggle-button' onClick={() => {setToggleView(!toggleView)}}>fwaf</div>
+    <ul>
+      {Object.keys(kanto).map((id) => {
+        return <li>{id}. {kanto[id]}</li>
+      })}
+    </ul>
+    </div>
   )
 }
-
-export default App
